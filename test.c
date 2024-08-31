3addbab31ac4187c8d29f6d41f8d3d42f87f761b
Suju <notsuju@proton.me>
2024-08-31 18:58:06 +0530

    New

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 2;
+    if (a = 5)
+    {
+        printf("Hello\n");
+    }
+    else
+    {
+        printf("Bye\n");
+    }
+    return 0;
+} 
