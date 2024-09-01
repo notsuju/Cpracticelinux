7363d84b716c810a9049a6cebf4f28fa1d8c7ab3
Suju <notsuju@proton.me>
2024-09-01 13:21:50 +0530

    "improved"

--- loops/noofdigits.c
+++ loops/noofdigits.c
@@ -5,12 +5,15 @@ int main()
     int a;
     printf("Enter the number: ");
     scanf("%d", &a);
+    int x = 0;
     int c = 0;
     while(a != 0)
     {
-        a /= 10;
+        x += a%10; //adds the last digit
+        a /= 10; // removes the last digit
         c++;
     }
     printf("The number is %d digits long\n", c);
+    printf("The sum of the digits of number is: %d\n", x);
     return 0;
 }
