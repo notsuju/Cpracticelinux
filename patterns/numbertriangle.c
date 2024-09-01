3d72b092ff7e43b1bcfe83b87a425ee225124ce8
Suju <notsuju@proton.me>
2024-09-01 21:44:33 +0530

    "improved"

--- patterns/numbertriangle.c
+++ patterns/numbertriangle.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main()
-{   
+{
     int a;
-    printf("Enter the number of rows : ");
+    printf("Enter the number of lines : ");
     scanf("%d", &a);
-    for(int i = a; i >= 1; i--)
+    for(int i = 1; i <= a; i++)
     {
         for(int j = 1; j <= i; j++)
         {
-            printf("* ");
+            printf("%d ", j);
         }
         printf("\n");
     }
