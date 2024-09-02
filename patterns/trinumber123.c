c7dc574e98d889e33bb661b840c175f6d42b300b
Suju <notsuju@proton.me>
2024-09-02 18:23:42 +0530

    "high"

--- patterns/trinumber123.c
+++ patterns/trinumber123.c
@@ -6,14 +6,23 @@ int main()
     printf("Enter number of lines : ");
     scanf("%d", &a);
     int x = 1;
+    // for(int i = 1; i <= a; i++)
+    // {
+    //     x = 1;
+    //     for(int j = 1; j <= i; j++)
+    //     {
+    //         printf("%d ", x);
+    //         x += 2;
+    //     }
+    //     printf("\n");
+    // }
     for(int i = 1; i <= a; i++)
     {
-        x = 1;
-        for(int j = 1; j <= i; j++)
+        for(int j = 1; j <= 2*i-1; j += 2)
         {
-            printf("%d ", x);
-            x += 2;
+            printf("%d ", j);
         }
         printf("\n");
     }
+    return 0;
 }
