f547fc26c60a03c4a122bb67edf05547bffbb681
Suju <notsuju@proton.me>
2024-09-02 21:56:28 +0530

    "bed low light"

---
+++ patterns/01triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows : ");
+    scanf("%d", &n);
+    /*
+    i=1    1
+    i=2    01
+    i=3    101
+    i=4    0101
+    */
+    int x;
+    for(int i = 1; i <= n; i++)
+    {
+        if(i%2 == 0) x = 0; // checks if i is even if it is then start with 0
+        else x = 1;
+        for(int j = 1; j <= i; j++)
+        {
+            if(x == 1) printf("%d", x--);
+            else printf("%d", x++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
