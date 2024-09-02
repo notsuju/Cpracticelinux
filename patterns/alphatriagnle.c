b508f65b3de48cb67b094da4daa360b41b48941d
Suju <notsuju@proton.me>
2024-09-02 18:37:41 +0530

    "reason"

---
+++ patterns/alphatriagnle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter the number of rows: ");
+    scanf("%d", &a);
+    for(int i = 1; i <= a; i++)
+    {
+        for(int j = 65; j <= i+64; j++)
+        {
+            printf("%c ", j);
+        }
+        printf("\n");
+    }
+    return 0; 
+}
