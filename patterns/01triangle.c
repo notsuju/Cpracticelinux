532fdcee92a7f2881e3647d187ccb7757bcfa7ec
Suju <notsuju@proton.me>
2024-09-02 22:24:44 +0530

    "news to me"

--- patterns/01triangle.c
+++ patterns/01triangle.c
@@ -11,15 +11,24 @@ int main()
     i=3    101
     i=4    0101
     */
-    int x;
+    // int x; (my version)
+    // for(int i = 1; i <= n; i++)
+    // {
+    //     if(i%2 == 0) x = 0; // checks if i is even if it is then start with 0
+    //     else x = 1;
+    //     for(int j = 1; j <= i; j++)
+    //     {
+    //         if(x == 1) printf("%d", x--);
+    //         else printf("%d", x++);
+    //     }
+    //     printf("\n");
+    // }
     for(int i = 1; i <= n; i++)
     {
-        if(i%2 == 0) x = 0; // checks if i is even if it is then start with 0
-        else x = 1;
         for(int j = 1; j <= i; j++)
         {
-            if(x == 1) printf("%d", x--);
-            else printf("%d", x++);
+            if(i == j || (i+j)%2 == 0) printf("1 ");
+            else printf("0 ");
         }
         printf("\n");
     }
