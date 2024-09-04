8b982fc8a0dcaba5a6a45f7ced32581cd8d7b6f1
Suju <notsuju@proton.me>
2024-09-04 23:13:15 +0530

    "back"

--- patterns/hardrectangle.c
+++ patterns/hardrectangle.c
@@ -5,39 +5,6 @@ int main()
     int n;
     printf("Enter the number : ");
     scanf("%d", &n);
-    // int c = n;
-
-    // for(int i = 1; i <= 2*n-1; i++)
-    // {
-    //     for(int j  = 1; j <= 2*n-1; j++)
-    //     {
-    //         // for(k = 1; k <= n; k++)
-    //         // {
-
-    //         // }
-    //         c = n;
-    //         if(j == 1 || j == 2*n-1 || i == 1 || i == 2*n-1)
-    //         {
-    //             printf("%d ", c);
-    //         }
-    //         else if (j == 2 || j == 2*n-2 || i == 2 || i == 2*n-2)
-    //         {
-    //             if(c == 4) c--;
-    //             printf("%d ", c);
-    //         }
-    //         else if(j == 3 || j == 2*n-3 || i == 3 || i == 2*n-3)
-    //         {
-    //             if(c == 4) c -= 2;
-    //             printf("%d ", c);
-    //         }
-    //         else if(j == i)
-    //         {
-    //             if(c == 4) c -= 3;
-    //             printf("%d ", c);
-    //         }
-    //     }
-    //     printf("\n");
-    // }
 
     int min;
     for(int i = 1; i <= 2*n-1; i++)
@@ -53,10 +20,5 @@ int main()
         }
         printf("\n");
     }
-
-
-
-
-
     return 0;
 }
