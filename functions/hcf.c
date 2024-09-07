cd7730fbd321d447e8b15a47dfad1db7f458269e
Suju <notsuju@proton.me>
2024-09-07 14:48:48 +0530

    "age of fluctuation"

---
+++ functions/hcf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int highest_factor(int x, int y);
+int main()
+{
+    int a,b;
+    printf("Enter the first number : ");
+    scanf("%d", &a);
+    printf("Enter the second number : ");
+    scanf("%d", &b);
+    printf("The highest common factor is : %d\n", highest_factor(a,b));
+    return 0;
+}
+
+int highest_factor(int x, int y)
+{
+    int hcf;
+    int min = (x > y)? y:x;
+    for(int i = min; i >= 2; i--)
+    {
+        if(x%i == 0 && y%i == 0)
+        {
+            hcf = i;
+            break;
+        }
+    }
+    return hcf;
+}
