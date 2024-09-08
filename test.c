c61b41cd690e9356ce6aff4d7e1f81d4892c811c
Suju <notsuju@proton.me>
2024-09-08 20:18:46 +0530

    "patterns"

--- test.c
+++ test.c
@@ -3,15 +3,9 @@
 
 int main()
 {
-    char s[25];
+    char *s;
     printf("Enter your name : ");
-    if (fgets(s, sizeof(s), stdin) != NULL) // checks if there is a error in reading ? 
-    // FGETS AUTOMATICALLY ADDS A \n in the end of the string
-    {
-        size_t x = strlen(s); // gets the length of the s
-        if (x > 0) s[x - 1] = '\0'; // checks if the length is more than 0 and if it is then replaces the '\n' with '\0'
-        printf("%d\n", x);
-    }
+    scanf("%s", &s);
     printf("Your name is : %s\n", s);
     return 0;
 } 
