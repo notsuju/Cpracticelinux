883e72cd0e54c36785fe358366b7d128425a94ce
Suju <notsuju@proton.me>
2024-09-13 19:37:54 +0530

    "chat"

--- arrays/maxarray.c
+++ arrays/maxarray.c
@@ -2,14 +2,21 @@
 
 int main()
 {
-    int a[5];
+    size_t size;
+    do
+    {
+        printf("Enter the size of the array : ");
+        scanf("%d", &size);
+    } while (size < 2);
+    
+    int a[size];
     int max;
-    for (int i = 0; i < 5; i++) // tried to take input and cal. max at the same time
+    for (int i = 0; i < size; i++) // tried to take input and cal. max at the same time
     {
         printf("Enter the number : ");
         scanf("%d", &a[i]);
         if (i == 0) max = a[i];
-        max = (max >= a[i])? max : a[i];
+        max = (max < a[i])? a[i]:max;
     }
     printf("%d is the maximum\n", max);
     return 0;
