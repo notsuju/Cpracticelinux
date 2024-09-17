b993d35b773644506430952685569128c12c605d
Suju <notsuju@proton.me>
2024-09-17 21:35:04 +0530

    "I wish I was there"

--- arrays/uniquearray.c
+++ arrays/uniquearray.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
+
+#define ARRAY_SIZE 7
 
 int main()
 {
-    int a[7] = {1,3,2,3,1,2,4};
+    int a[ARRAY_SIZE] = {1,3,2,3,1,2,4};
     for(int i = 0; i < 7; i++)
     {
         bool flag = false;
@@ -23,3 +26,34 @@ int main()
     }
     return 0;
 }
+
+// chatgpt version using an array as counting the times a element comes in the array
+// #include <stdio.h>
+// #include <limits.h>
+
+// #define ARRAY_SIZE 7
+// #define MAX_VALUE 10 // Should be more than the maximum value in array
+
+// int main()
+// {
+//     int a[ARRAY_SIZE] = {1, 3, 2, 3, 1, 2, 4};
+//     int count[MAX_VALUE] = {0}; // Initialize counting array with 0s
+
+//     // Count occurrences of each number
+//     for(int i = 0; i < ARRAY_SIZE; i++)
+//     {
+//         count[a[i]]++;
+//     }
+
+//     // Find and print the unique number
+//     for(int i = 0; i < ARRAY_SIZE; i++)
+//     {
+//         if(count[a[i]] == 1)
+//         {
+//             printf("%d is the unique number in the given array\n", a[i]);
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
