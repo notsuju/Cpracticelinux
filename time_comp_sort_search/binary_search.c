101fc8d830ab1a7feeb4006abe0979d577f5275e
Suju <notsuju@proton.me>
2024-10-11 19:15:56 +0530

    "updated some things"

--- time_comp_sort_search/binary_search.c
+++ time_comp_sort_search/binary_search.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+// Time complexity of O(n)
 int main()
 {
+    // Taking size of array as input
     int n;
     printf("Enter the number of elements in the array : ");
     scanf("%d", &n);
@@ -11,24 +12,27 @@ int main()
         printf("Error the number of elements should be positive\n");
         return 1;
     }
-
+    // Allocating memory for the array
     int* arr = (int* )malloc(n * sizeof(int));
     if(arr == NULL)
     {
         printf("Error allocating memory\n");
         return 2;
     }
+    
+    // Taking array as input
     for(int i = 0; i < n; i++)
     {
         printf("Enter a number for index %d : ", i);
         scanf("%d", &arr[i]);
     }
 
+    // Number to be searched
     int s;
     printf("Enter the number to be searched : ");
     scanf("%d", &s);
 
-
+    // BInary search algorithm
     int half_max = n - 1;
     int half_min = 0;
     int mid;
@@ -46,6 +50,8 @@ int main()
     };
     if(found == -1) printf("Error searching\n");
     else printf("Found at index %d\n", found);
+
+    // Freeing allocated memory
     free(arr);
     arr = NULL;
     return 0;
